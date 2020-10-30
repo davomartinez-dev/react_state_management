3b2be41728c66bcd7c98bf50ea552a815cba1480
davomartinez-dev <davidelimg1@gmail.com>
2020-10-30 14:28:33 -0600

    Implementing deriving state

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Footer from './Footer';
 import Header from './Header';
@@ -44,6 +44,8 @@ const products = [
 ];
 
 export default function App() {
+  const [size, setSize] = useState('');
+
   function renderProduct(p) {
     return (
       <div key={p.id} className="product">
@@ -63,7 +65,11 @@ export default function App() {
         <main>
           <section id="filters">
             <label htmlFor="size">Filter by Size:</label>{' '}
-            <select id="size">
+            <select
+              id="size"
+              value={size}
+              onChange={e => setSize(e.target.value)}
+            >
               <option value="">All sizes</option>
               <option value="7">7</option>
               <option value="8">8</option>
