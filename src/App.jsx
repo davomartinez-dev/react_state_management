de6bac654659dedda2e25ecda39c522aa1da1813
davomartinez-dev <davidelimg1@gmail.com>
2020-11-10 10:13:47 -0600

    Adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import './App.css';
 import Footer from './Footer';
 import Header from './Header';
 import Products from './Products';
+import { Routes, Route } from 'react-router-dom';
+import Detail from './Detail';
+import Cart from './Cart';
 
 export default function App() {
   return (
