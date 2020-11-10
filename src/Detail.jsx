4ca8343007e6bb2b583ef1c26d0bd63f1715eb06
davomartinez-dev <davidelimg1@gmail.com>
2020-11-10 16:15:21 -0600

    Added routing

--- src/Detail.jsx
+++ src/Detail.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import PageNotFound from './PageNotFound';
+import useFetch from './services/useFetch';
+import Spinner from './Spinner';
 
 export default function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const navigate = useNavigate();
+  const { data: product, loading, error } = useFetch(`products/${id}`);
 
-  // TODO: Display these products details
-  // return (
-  //   <div id="detail">
-  //     <h1>{product.name}</h1>
-  //     <p>{product.description}</p>
-  //     <p id="price">${product.price}</p>
-  //     <img src={`/images/${product.image}`} alt={product.category} />
-  //   </div>
-  // );
+  if (loading) return <Spinner />;
+  if (!product) return <PageNotFound />;
+  if (error) throw error;
+  return (
+    <div id="detail">
+      <h1>{product.name}</h1>
+      <p>{product.description}</p>
+      <p id="price">${product.price}</p>
+      <p>
+        <button className="btn btn-primary" onClick={() => navigate('/cart')}>
+          Add to Cart
+        </button>
+      </p>
+      <img src={`/images/${product.image}`} alt={product.category} />
+    </div>
+  );
 }
